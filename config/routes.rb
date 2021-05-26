f071ca1a6931544e314df5e6a795d42bf80d554f
Mitch <MitchelClark1997@outlook.com>
2021-05-26 22:11:17 +1000

    facebook omniauth basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 root to: 'users#index'
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'}
   resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
