c6707e693a712684e55d88cc1dabd3dc42c01011
Mitch <MitchelClark1997@outlook.com>
2021-06-06 20:35:11 +1000

    Invite model spec

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,17 +2,22 @@ class Invite < ApplicationRecord
   belongs_to :user_group, foreign_key: 'user_group_id'
   belongs_to :invitor, class_name: 'User', inverse_of: 'my_sent_invites'
   belongs_to :invitee, class_name: 'User', inverse_of: 'my_ask_invites'
-  # belongs_to :member,-> { where accepted: true, confirmed: true}, class_name: 'User'
+
+  validates :user_group_id, presence: :true
+  validates :invitor_id, presence: :true
+  validates :invitee_id, presence: :true
+  validates :accepted, presence: :true
+  validates :confirmed, presence: :true
   
   # Given an invite, find the actual non-host on Invite.
-  def worker
-    host = user_group.host
-    if invitor == host
-      invitee
-    else
-      invitor
-    end
-  end
+  # def worker
+  #   host = user_group.host
+  #   if invitor == host
+  #     invitee
+  #   else
+  #     invitor
+  #   end
+  # end
 
   def to_partial_path
     'invite/member'
--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(
+    name: 'mitch',
+    email: 'mitch@bing',
+    password: 'Bing111'
+  )}
+  let(:group) { UserGroup.new(
+    name: 'Coolies',
+    host_id: user.id
+  )}
+  subject { described_class.new(
+    invitor_id: user.id,
+    invitee_id: user.id,
+    user_group_id: group.id,
+    accepted: true,
+    confirmed: true
+  )}
+
+  describe "ASSOCIATIONS" do
+    it { should belong_to(:user_group) }
+    it { should belong_to(:invitor) }
+    it { should belong_to(:invitee) }
+  end
+
+  describe "VALIDATIONS" do
+    it { should validate_presence_of(:user_group_id) }
+    it { should validate_presence_of(:invitee_id) }
+    it { should validate_presence_of(:invitor_id) }
+    it { should validate_presence_of(:accepted) }
+    it { should validate_presence_of(:confirmed) }
+  end
+
+  describe "#to_partial_path" do
+    context 'give member partial as base default' do
+      it 'return string path' do
+        user.save!
+        group.save!
+        subject.save!
+        expect(subject.to_partial_path).to eql('invite/member')
+      end
+    end
+  end
+
 end
